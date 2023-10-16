e6a69ef90d1da3b003e88b99912f17635f6a4186
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-17 01:15:06 +0500

    Commenting the tutorial

--- queue.js
+++ queue.js
@@ -1,24 +1,37 @@
-// Playing with array
-// ADDING VALUE TO AN ARRAY
- 
-let myArray = [10, "Mohaamd"];
+// JavaScript tutorial playing with Arrays  
+// what is an Array
+
+// Why use Array
 
 
-// HOW WE CAN ADD VALUE TO THE END OF AN ARRAY
-myArray.push(20)
-console.log(myArray)
-myArray.push("Jon")
-console.log(myArray)
+//Ordered Collection: Arrays allow you to store multiple values in an ordered collection. 
+// You can access and manipulate these values by their position (index) within the array.
 
-// HOW WE CAN ADD VALUE TO THE BEGGING OF AN ARRAY
-myArray.unshift(30)
-console.log(myArray)
-myArray.unshift("Mike")
-console.log(myArray)
+// Lists: Arrays are often used to represent lists of items. 
+// For example, you can use an array to store a list of names, to-do tasks, or items in a shopping cart.
 
-// HOW WE CAN USE INDEXING TO ADD VALUE IN MIDDLE OR DEFERNT POSSITION OF THE ARRAY
-const atWhichIndex = 3;
-const valueAddToArray = "Mohammad";
-myArray.splice(atWhichIndex, 0, valueAddToArray)
-console.log(myArray)
+// Iteration: Arrays are well-suited for iterating over a set of values. 
+//You can use loops (e.g., for loops or forEach) to iterate through the elements in an array.
 
+// Storing Data: Arrays can store different types of data, such as numbers, strings, objects, or a combination of these. 
+//This makes them versatile for various data storage needs.
+
+// Sorting and Searching: Arrays provide methods and algorithms for sorting and searching elements efficiently. 
+//For example, you can sort an array in ascending or descending order, or you can search for a specific value within an array.
+
+// Stacks and Queues: 
+//Arrays can be used to implement data structures like stacks (Last-In-First-Out) and queues (First-In-First-Out) for managing data and processing tasks.
+
+// Implementing Data Structures: Many other data structures, like linked lists, hash tables, and graphs, can be implemented using arrays or arrays of arrays.
+
+// Data Transformation: Arrays can be used to transform and manipulate data. 
+//For example, you can map, filter, and reduce arrays to create new arrays or extract specific information.
+
+// Mathematical Operations: Arrays are commonly used in mathematical and scientific programming to perform operations on large datasets.
+
+// Database Operations: In some cases, arrays are used to store and manage data retrieved from databases or web services.
+
+// Dynamic Data: Arrays can be used to handle dynamic data, where the size of the collection may change during the execution of a program.
+
+// Efficient Access: Arrays provide efficient access to elements based on their index. 
+//This makes them useful when you need to retrieve or modify specific elements quickly.
