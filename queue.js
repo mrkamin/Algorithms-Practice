8600848f39c2b112cdd74495a13f15cb0cf618bc
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-01 17:47:00 +0500

    How creat an array from another array

--- queue.js
+++ queue.js
@@ -4,3 +4,8 @@ console.log(myArray)// output = []
 const arrayOfStringNumbers = ["one", "two", "three"]
 console.log(arrayOfStringNumbers); // output = [ 'one', 'two', 'three' ]
 console.log(arrayOfStringNumbers[2]); // output = three
+
+const myNewArray = Array.from(myArray);
+console.log(myNewArray); // output = []
+const my2NewArray = Array.from(arrayOfStringNumbers);
+console.log(my2NewArray); // output = [ 'one', 'two', 'three' ]
