4baa876331dced62060f0437e840750ab427614a
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-01 18:43:55 +0500

    Ues of concat method

--- queue.js
+++ queue.js
@@ -1,12 +1,5 @@
-const myArray = new Array();
-console.log(myArray)// output = []
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
 
-const arrayOfStringNumbers = ["one", "two", "three"]
-console.log(arrayOfStringNumbers); // output = [ 'one', 'two', 'three' ]
-console.log(arrayOfStringNumbers[2]); // output = three
-
-const myNewArray = Array.from(myArray);
-console.log(myNewArray); // output = []
-const my2NewArray = Array.from(arrayOfStringNumbers);
-console.log(my2NewArray); // output = [ 'one', 'two', 'three' ]
-console.log(my2NewArray[1]); // output = two
+const array3 = array1.concat(array2);
+console.log(array3) // output = [ 1, 2, 3, 4, 5, 6 ]
