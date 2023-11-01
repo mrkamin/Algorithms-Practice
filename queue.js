406e7cd7e7a565fdf38015ffbedc8855360a55e0
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-01 17:39:11 +0500

    Array Creation

---
+++ queue.js
@@ -0,0 +1,6 @@
+const myArray = new Array();
+console.log(myArray)// output = []
+
+const arrayOfStringNumbers = ["one", "two", "three"]
+console.log(arrayOfStringNumbers); // output = [ 'one', 'two', 'three' ]
+console.log(arrayOfStringNumbers[2]); // output = three
