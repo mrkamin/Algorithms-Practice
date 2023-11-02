9acbd56cee99ad95f139329e1c78e0385417f713
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-02 17:37:29 +0500

    use .reduce method

--- queue.js
+++ queue.js
@@ -1,4 +1,3 @@
-const fruits = ["apple", "banana", "orange", "kiwi", "mango"];
-const fruitsWithLengthArray = fruits.map((fruit) => fruit.length);
-console.log(fruitsWithLengthArray); // output = [ 5, 6, 6, 4, 5 ]
-
+const numbers = [1,2,3,4,5];
+const sumOfNumbers = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(sumOfNumbers); // output = 15
