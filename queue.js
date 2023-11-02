94b1bdb395242ff5d1c64b20ae24f410fdbb25ff
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-02 17:12:36 +0500

    use .map method

--- queue.js
+++ queue.js
@@ -1,3 +1,4 @@
 const fruits = ["apple", "banana", "orange", "kiwi", "mango"];
 const fruitsWithLengthArray = fruits.map((fruit) => fruit.length);
 console.log(fruitsWithLengthArray); // output = [ 5, 6, 6, 4, 5 ]
+
