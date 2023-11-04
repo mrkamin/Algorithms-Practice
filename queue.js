eeec131b0383294f3c15a53ffb67d7a1007657f9
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-04 15:02:42 +0500

    Array.from Method

--- queue.js
+++ queue.js
@@ -1,4 +1,9 @@
-let arrOfObjects = [{name: "ahmad", age: 32}, {naem: "Rafi", age: 33}]
-console.log(arrOfObjects[0])
-let arrOfArrays = [[1,2,3,4], [5,6,7,8]]
-console.log(arrOfArrays[1])
+let myArray = new Array();
+console.log(myArray)
+let arrOfNumbers = new Array(1,2,3,4,5)
+console.log(arrOfNumbers)
+// create an array of strings wiht valus of ["Ahmad", "Rafi", "Mohammad"]
+let arrOfStrings = new Array("Ahmad", "Rafi", "Mohammad")
+console.log(arrOfStrings)
+let arrFromArrOfStrings = Array.from(arrOfStrings)
+console.log("arrFromArrOfStrings", arrFromArrOfStrings)
