95f6d038882fca860cbaf72598cdcc74a5aed5ce
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-04 21:58:25 +0500

    array

--- queue.js
+++ queue.js
@@ -1,9 +1,9 @@
-let myArray = new Array();
-console.log(myArray)
-let arrOfNumbers = new Array(1,2,3,4,5)
-console.log(arrOfNumbers)
-// create an array of strings wiht valus of ["Ahmad", "Rafi", "Mohammad"]
-let arrOfStrings = new Array("Ahmad", "Rafi", "Mohammad")
-console.log(arrOfStrings)
-let arrFromArrOfStrings = Array.from(arrOfStrings)
-console.log("arrFromArrOfStrings", arrFromArrOfStrings)
+let arrOfNumbers = [1,2,3,4,5]
+let arrOfStrings = ["A","B", "C"]
+let arrOfArrays = [[1,2,3,4,5], ["A","B", "C"]]
+let arrOfObjects = [{name: "Mohammad", age: 33}, {name: "Rafi", age: 25}]
+
+console.log(arrOfNumbers[0]);
+console.log(arrOfStrings[1]);
+console.log(arrOfArrays[0]);
+console.log(arrOfObjects[1])
